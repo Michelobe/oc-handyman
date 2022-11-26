0cf722d22c689b9516d198ceb66adab39243ed6f
Michael <michaeldlr17@gmail.com>
2022-11-25 21:48:16 -0800

    Styling up the footer, I have less to put in the footer than the example, might have to get creative with the layout. committing changes for this session and pushing to github.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,33 @@ const Footer = () => {
         <section className='footerSection'>
             <div className='container'>
                 <div className='footerCta'>
-                    <h1>Footer area !!!!!!!!!!!!!!!</h1>
+                    <h1>Check out the yelp page</h1>
                 </div>
                 <div className='footerContent'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className='footerCol'>
+                        <h2 className='footerHeader'>OC Handyman</h2>
+                        <p className='columnContent'>We have the quality work you expect.</p>
+                    </div>
+                    <div className='footerCol'>
+                        <h2 className='footerHeader'>Useful Links</h2>
+                        <div className='columnLinks'>
+                            <ul className='columnContent'>
+                                <li>About</li>
+                                <li>FAQ</li>
+                                <li>Contact Us</li>
+                                <li>Locations</li>
+                            </ul>
+                            <ul className='columnContent'>
+                                <li>Services</li>
+                                <li>Gallery</li>
+                                <li>Reviews</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='footerCol'>
+                        <h2 className='footerHeader'>Contact Us</h2>
+                        <p className='columnContent'>contact Info</p>
+                    </div>
                 </div>
             </div>
         </section>
