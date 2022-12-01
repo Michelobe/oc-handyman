f25332364dfbe7e79972469e2f024e0f7caee951
Michael <michaeldlr17@gmail.com>
2022-11-30 20:53:24 -0800

    Optimizing fo r all platforms, jumbotron complete. Working on next section, the card section. This section will always be a chalenge depending on the wording in the containers. committing and pushing changes for this session.

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -7,26 +7,26 @@ const CardSection = () => {
           <div className='container cardContainer'>
               <div className='card'>
                   <div className='iconContainer'>
-                    <i className="fas fa-cog"></i>
+                    <i className="fas fa-user-tie"></i>
                   </div>
-                  <h1>Large Number of Services Provided</h1>
-                  <p>We are a company providing all kinds of services throughout OC.</p>
+                  <h1>10+ Years Professional Experience</h1>
+                  <p>Extensive experience allows us to get the job done right.</p>
                   <button>More Services</button>
               </div>
               <div className='card'>
                   <div className='iconContainer'>
-                    <i className="fas fa-user-tie"></i>
+                    <i className="fas fa-cog"></i>
                   </div>
-                  <h1>Large Number of Services Provided</h1>
-                  <p>We are a company providing all kinds of services throughout OC.</p>
+                  <h1>Multiple Services Available For Every Need</h1>
+                  <p>Our team has the knowledge & skills for tasks you need.</p>
                   <button>More Services</button>
               </div>
               <div className='card'>
                   <div className='iconContainer'>
                     <i className="fas fa-thumbs-up"></i>
                   </div>
-                  <h1>Large Number of Services Provided</h1>
-                  <p>We are a company providing all kinds of services throughout OC.</p>
+                  <h1>The Trust Customers Can Always Depend Upon</h1>
+                  <p>Providing happy customers throughout Orange County.</p>
                   <button>More Services</button>
               </div>
           </div>
