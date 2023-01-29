5dccc4d10a649a8e3a9539c507a1836dd53f5152
Michael <michaeldlr17@gmail.com>
2023-01-29 09:15:38 -0800

    I am working on the projects section latest job selection, there is some responsiveness that needs to be addressed with the filter options now working. Committing changes to test live and then i will address the responsiveness.  Also, I should add a tile to say ther is not an option the user has chosen.

--- src/components/homePage/sections/projectsSection/ProjectsSection.js
+++ src/components/homePage/sections/projectsSection/ProjectsSection.js
@@ -4,9 +4,140 @@ import projectsData from './projectsInfo.js';
 
 const ProjectsSection = () => {
     const projectInformation = useState(projectsData);
-    const projectArray = projectInformation[0];
-    console.log(projectArray);
+    var projectArray = projectInformation[0];
 
+    // SET STATE FOR BUTTONS
+    var [electrical, setElectrical] = useState(true);
+    var [cabinetry, setCabinetry] = useState(true);
+    var [painting, setPainting] = useState(true);
+    var [plumbing, setPlumbing] = useState(true);
+    var [flooring, setFlooring] = useState(true);
+    var [maintenance, setMaintenance] = useState(true);
+
+
+
+    var toggleAll = () => {
+        setElectrical(true);
+        setCabinetry(true);
+        setPainting(true);
+        setPlumbing(true);
+        setFlooring(true);
+        setMaintenance(true);
+
+        for(var i=0;i < projectArray.length;i++){
+            projectArray[i].toggleActive=true;
+        }
+    };
+    
+    var electricalOption = () => {
+        setElectrical(true);
+        setCabinetry(false);
+        setPainting(false);
+        setPlumbing(false);
+        setFlooring(false);
+        setMaintenance(false);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='electrical'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
+
+    var cabinetryOption = () => {
+        setElectrical(false);
+        setCabinetry(true);
+        setPainting(false);
+        setPlumbing(false);
+        setFlooring(false);
+        setMaintenance(false);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='cabinetry'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
+    var paintingOption = () => {
+        setElectrical(false);
+        setCabinetry(false);
+        setPainting(true);
+        setPlumbing(false);
+        setFlooring(false);
+        setMaintenance(false);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='painting'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
+    var plumbingOption = () => {
+        setElectrical(false);
+        setCabinetry(false);
+        setPainting(false);
+        setPlumbing(true);
+        setFlooring(false);
+        setMaintenance(false);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='plumbing'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
+    var flooringOption = () => {
+        setElectrical(false);
+        setCabinetry(false);
+        setPainting(false);
+        setPlumbing(false);
+        setFlooring(true);
+        setMaintenance(false);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='flooring'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
+    var maintenanceOption = () => {
+        setElectrical(false);
+        setCabinetry(false);
+        setPainting(false);
+        setPlumbing(false);
+        setFlooring(false);
+        setMaintenance(true);
+
+        console.log(projectArray);
+        for(var i=0;i < projectArray.length;i++){
+            if(projectArray[i].type==='maintenance'){
+                projectArray[i].toggleActive=true;
+            }
+            else{
+                projectArray[i].toggleActive=false;
+            }
+        }
+    };
 
     return (
         <section className='projectsSection'>
@@ -17,46 +148,47 @@ const ProjectsSection = () => {
                         <h2>OCHandyman's Latest</h2>
                     </header>
                     <nav className='mobileNav'>
-                        <select className='navDrop' name="job" id="job">
-                            <option className='dropOption' value="All" type='button' data-btn='1'>All</option>
-                            <option className='dropOption' value="Electrical" type='button' data-btn='2'>Electrical</option>
-                            <option className='dropOption' value="Cabinetry" type='button' data-btn='3'>Cabinetry</option>
-                            <option className='dropOption' value="Painting" type='button' data-btn='4'>Painting</option>
-                            <option className='dropOption' value="Plumbing" type='button' data-btn='5'>Plumbing</option>
-                            <option className='dropOption' value="Flooring" type='button' data-btn='6'>Flooring</option>
-                            <option className='dropOption' value="Maintenance" type='button' data-btn='7'>Maintenance</option>
-                        </select>
+                        <ul className='navDrop' name="job" id="job">
+                            <li className='dropOption' value="All" type='button'>Select</li>
+                            <li className='dropOption' value="All" type='button' onClick={toggleAll}>All</li>
+                            <li className='dropOption' value="Electrical" type='button' onClick={electricalOption}>Electrical</li>
+                            <li className='dropOption' value="Cabinetry" type='button' onClick={cabinetryOption}>Cabinetry</li>
+                            <li className='dropOption' value="Painting" type='button' onClick={paintingOption}>Painting</li>
+                            <li className='dropOption' value="Plumbing" type='button' onClick={plumbingOption}>Plumbing</li>
+                            <li className='dropOption' value="Flooring" type='button' onClick={flooringOption}>Flooring</li>
+                            <li className='dropOption' value="Maintenance" type='button' onClick={maintenanceOption}>Maintenance</li>
+                        </ul>
                     </nav>
                     <div className='projectsNavBtns'>
                         <ul className='navBtns'>
                             <li>
-                                <button className='projectButton' type='button' data-btn='1'>All</button>
+                                <button className='projectButton' type='button' onClick={toggleAll}>All</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='2'>Electrical</button>
+                                <button className='projectButton' type='button' onClick={electricalOption}>Electrical</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='3'>Cabinetry</button>
+                                <button className='projectButton' type='button' onClick={cabinetryOption}>Cabinetry</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='4'>Painting</button>
+                                <button className='projectButton' type='button' onClick={paintingOption}>Painting</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='5'>Plumbing</button>
+                                <button className='projectButton' type='button' onClick={plumbingOption}>Plumbing</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='6'>Flooring</button>
+                                <button className='projectButton' type='button' onClick={flooringOption}>Flooring</button>
                             </li>
                             <li>
-                                <button className='projectButton' type='button' data-btn='7'>Maintenance</button>
+                                <button className='projectButton' type='button' onClick={maintenanceOption}>Maintenance</button>
                             </li>
                         </ul>
                     </div>
                 </nav>
 
 
-                <div className='projectTiles'>
-                    <div className='projectTile'>
+                <div className={`projectTiles`} >
+                    <div className={`projectTile ${projectArray[0].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
                             <img src={projectArray[0].img} alt={projectArray[0].alt} />
                         </div>
@@ -71,76 +203,76 @@ const ProjectsSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='projectTile'>
+                    <div className={`projectTile ${projectArray[1].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
-                            <img src={projectInformation[0][1].img} alt={projectInformation[0][1].alt} />
+                            <img src={projectArray[1].img} alt={projectArray[1].alt} />
                         </div>
                         <div className='tileHover'>
                             <div className="tileHoverPopup">
                                 <div className="tileShade"></div>
                                 <div className='tileContent'>
-                                    <h6>{projectInformation[0][1].header}</h6>
-                                    <h2>{projectInformation[0][1].location}</h2>
+                                    <h6>{projectArray[1].header}</h6>
+                                    <h2>{projectArray[1].location}</h2>
                                     <div className='tileBottomBorder'></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='projectTile'>
+                    <div className={`projectTile ${projectArray[2].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
-                            <img src={projectInformation[0][2].img} alt={projectInformation[0][2].alt} />
+                            <img src={projectArray[2].img} alt={projectArray[2].alt} />
                         </div>
                         <div className='tileHover'>
                             <div className="tileHoverPopup">
                                 <div className="tileShade"></div>
                                 <div className='tileContent'>
-                                    <h6>{projectInformation[0][2].header}</h6>
-                                    <h2>{projectInformation[0][2].location}</h2>
+                                    <h6>{projectArray[2].header}</h6>
+                                    <h2>{projectArray[2].location}</h2>
                                     <div className='tileBottomBorder'></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='projectTile'>
+                    <div className={`projectTile ${projectArray[3].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
-                            <img src={projectInformation[0][3].img} alt={projectInformation[0][3].alt} />
+                            <img src={projectArray[3].img} alt={projectArray[3].alt} />
                         </div>
                         <div className='tileHover'>
                             <div className="tileHoverPopup">
                                 <div className="tileShade"></div>
                                 <div className='tileContent'>
-                                    <h6>{projectInformation[0][3].header}</h6>
-                                    <h2>{projectInformation[0][3].location}</h2>
+                                    <h6>{projectArray[3].header}</h6>
+                                    <h2>{projectArray[3].location}</h2>
                                     <div className='tileBottomBorder'></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='projectTile'>
+                    <div className={`projectTile ${projectArray[4].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
-                            <img src={projectInformation[0][4].img} alt={projectInformation[0][4].alt} />
+                            <img src={projectArray[4].img} alt={projectArray[4].alt} />
                         </div>
                         <div className='tileHover'>
                             <div className="tileHoverPopup">
                                 <div className="tileShade"></div>
                                 <div className='tileContent'>
-                                    <h6>{projectInformation[0][4].header}</h6>
-                                    <h2>{projectInformation[0][4].location}</h2>
+                                    <h6>{projectArray[4].header}</h6>
+                                    <h2>{projectArray[4].location}</h2>
                                     <div className='tileBottomBorder'></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='projectTile'>
+                    <div className={`projectTile ${projectArray[5].toggleActive ? '' : 'nonActive'}`}>
                         <div className='tilePhoto'>
-                            <img src={projectInformation[0][5].img} alt={projectInformation[0][5].alt} />
+                            <img src={projectArray[5].img} alt={projectArray[5].alt} />
                         </div>
                         <div className='tileHover'>
                             <div className="tileHoverPopup">
                                 <div className="tileShade"></div>
                                 <div className='tileContent'>
-                                    <h6>{projectInformation[0][5].header}</h6>
-                                    <h2>{projectInformation[0][5].location}</h2>
+                                    <h6>{projectArray[5].header}</h6>
+                                    <h2>{projectArray[5].location}</h2>
                                     <div className='tileBottomBorder'></div>
                                 </div>
                             </div>
