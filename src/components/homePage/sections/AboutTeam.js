293d12fedb1ae6ec13f0966d5faccf61ad787899
Michael <michaeldlr17@gmail.com>
2023-02-14 19:32:45 -0800

    After adding another handyman, the reponsiveness was way off on the aboutTeam section.  It needed to be rectified immediately.  Committing changes and pushing to github.

--- src/components/homePage/sections/AboutTeam.js
+++ src/components/homePage/sections/AboutTeam.js
@@ -5,9 +5,9 @@ import Handyman from '../../../images/handyman-placeholder.png';
 const AboutTeam = () => {
     return (
         <section className='aboutTeam'>
-            <div className='container'>
-                <div className='sectionDownshift'>
-                    <header className='aboutHeader'>
+            <div className='sectionDownshift'>
+                <header>
+                    <div className='container aboutHeader'>
                         <div className='headerDiv'>
                             <h4>OC Local Handyman</h4>
                             <h2>Our skilled team</h2>
@@ -17,24 +17,24 @@ const AboutTeam = () => {
                                 Team members from all around the construction industry add professional experience to OC Local Handyman. 
                             </p>
                         </div>
-                    </header>
-                    <div className='teamIntro'>
-                        <Link to="/team" relative="path" className='teammate'>
-                            <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
-                            <h2>Steve De La Riva</h2>
-                            <h6>Handyman</h6>
-                        </Link>
-                        <Link to="/team" relative="path" className='teammate'>
-                            <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
-                            <h2>Brian Welsh</h2>
-                            <h6>Handyman</h6>
-                        </Link>
-                        <Link to="/team" relative="path" className='teammate'>
-                            <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
-                            <h2>Mario Puccio</h2>
-                            <h6>Handyman</h6>
-                        </Link>
                     </div>
+                </header>
+                <div className='teamIntro'>
+                    <Link to="/team" relative="path" className='teammate'>
+                        <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
+                        <h2>Steve De La Riva</h2>
+                        <h6>Handyman</h6>
+                    </Link>
+                    <Link to="/team" relative="path" className='teammate'>
+                        <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
+                        <h2>Brian Welsh</h2>
+                        <h6>Handyman</h6>
+                    </Link>
+                    <Link to="/team" relative="path" className='teammate'>
+                        <div className='teamPhoto'><img src={Handyman} alt="Handyman" /></div>
+                        <h2>Mario Puccio</h2>
+                        <h6>Handyman</h6>
+                    </Link>
                 </div>
             </div>
         </section>
